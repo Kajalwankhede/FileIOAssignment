96550d322dab1081d3c67862abecb7c9350967f3
Kajalwankhede <kajuwankhede1510@gmail.com>
2022-11-26 21:32:58 +0530

    Welcome To Employee PayRoll Service

---
+++ src/main/java/com/employeepayroll/EmployeePayRoll.java
@@ -0,0 +1,8 @@
+package com.employeepayroll;
+
+public class EmployeePayRoll {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee PayRoll Service");
+    }
+
+}
